7b03666847dd1b3d929c802af48ff1b9b8a01e4e
Baldish <baldish6singh@gmail.com>
2025-04-09 23:36:20 +0200

    connect to db

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
 
 /* CONFIGURATIONS */
-dotenv.config8ì();
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -15,4 +16,18 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit: "30mb", extended : true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended : true}));
 app.use(cors());
+
+/* MONGOOSE */
+
+const connect = () =>{
+    mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("connected to db ")
+    }).catch((err)=>{throw err;});
+}
+
+app.listen(8800,()=>{
+    connect()
+    console.log("connected to server ")
+})
+
 
